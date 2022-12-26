72709bcb886aab57add4edd5a7c8b1b848b9eac2
nilic <github@nilic.33mail.com>
2022-12-26 09:26:16 +0100

    refactor flags

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
@@ -32,46 +31,27 @@ func Execute() {
 }
 
 func init() {
-	debugCmd, err := getCmdWithFlags("debug")
-	if err != nil {
-		log.Fatalf("error: %v\n", err)
-	}
+	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDiscoveryBurst(350).WithDiscoveryQPS(50.0)
+	matchVersionKubeConfigFlags := kcmdutil.NewMatchVersionFlags(kubeConfigFlags)
+	f := kcmdutil.NewFactory(matchVersionKubeConfigFlags)
+	ioStreams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+
+	debugCmd := debug.NewCmdDebug(f, ioStreams)
 	debugCmd.SetHelpTemplate("TODO: debug help")
 	debugCmd.Flags().Set("image", "nicolaka/netshoot")
 	debugCmd.Flags().Set("stdin", "true")
 	debugCmd.Flags().Set("tty", "true")
 	rootCmd.AddCommand(debugCmd)
 
-	runCmd, err := getCmdWithFlags("run")
-	if err != nil {
-		log.Fatalf("error: %v\n", err)
-	}
+	runCmd := run.NewCmdRun(f, ioStreams)
 	runCmd.SetHelpTemplate("TODO: run help")
 	runCmd.Flags().Set("image", "nicolaka/netshoot")
 	runCmd.Flags().Set("stdin", "true")
 	runCmd.Flags().Set("tty", "true")
 	runCmd.Flags().Set("rm", "true")
 	rootCmd.AddCommand(runCmd)
-}
 
-func getCmdWithFlags(cmdName string) (*cobra.Command, error) {
-	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDiscoveryBurst(350).WithDiscoveryQPS(50.0)
-	matchVersionKubeConfigFlags := kcmdutil.NewMatchVersionFlags(kubeConfigFlags)
-	f := kcmdutil.NewFactory(matchVersionKubeConfigFlags)
-	ioStreams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
-	var cmd *cobra.Command
-	switch cmdName {
-	case "debug":
-		cmd = debug.NewCmdDebug(f, ioStreams)
-	case "run":
-		cmd = run.NewCmdRun(f, ioStreams)
-	default:
-		return nil, fmt.Errorf("invalid command type, expected (debug, run), got %s", cmdName)
-	}
-
-	kubeConfigFlags.AddFlags(cmd.PersistentFlags())
-	matchVersionKubeConfigFlags.AddFlags(cmd.PersistentFlags())
-	cmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
-
-	return cmd, nil
+	kubeConfigFlags.AddFlags(rootCmd.PersistentFlags())
+	matchVersionKubeConfigFlags.AddFlags(rootCmd.PersistentFlags())
+	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 }
