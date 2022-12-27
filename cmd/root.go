56aee0e209614e8ca7300391e159c1c60f98e78c
nilic <github@nilic.33mail.com>
2022-12-27 22:28:53 +0100

    ocd

--- cmd/root.go
+++ cmd/root.go
@@ -38,6 +38,7 @@ func init() {
 		"host-network", false, "(applicable to \"run\" only) whether to spin up netshoot on the host's network namespace")
 	rootCmd.PersistentFlags().StringVar(&imageTag,
 		"image-tag", "latest", "netshoot container image tag to use")
+
 	kubeConfigFlags := genericclioptions.NewConfigFlags(true).WithDiscoveryBurst(350).WithDiscoveryQPS(50.0)
 	matchVersionKubeConfigFlags := kcmdutil.NewMatchVersionFlags(kubeConfigFlags)
 	f := kcmdutil.NewFactory(matchVersionKubeConfigFlags)
